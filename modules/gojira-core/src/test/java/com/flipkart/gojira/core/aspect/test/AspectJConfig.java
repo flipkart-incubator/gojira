ea64aeb09c1844790e87d550fe976f765686011b
Bala Lakshmi <35446908+balalakshmifk@users.noreply.github.com>
2020-04-24 10:16:36 +0530

    Aspectj integration (#51)
    * Adding aspectj integration
    
    * Adding docs
    
    * adding maven surefire plugin for fixing classpath issue
    
    * review comments
    
    * version bump
    
    Co-authored-by: arunachalams <arunachalam.s@flipkart.com>
    Co-authored-by: Parth Shyara <pgs2496@gmail.com>

---
+++ modules/gojira-core/src/test/java/com/flipkart/gojira/core/aspect/test/AspectJConfig.java
@@ -0,0 +1,34 @@
+/*
+ *
+ * Copyright 2020 Flipkart Internet, pvt ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http:www.apache.orglicensesLICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.flipkart.gojira.core.aspect.test;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+public @interface AspectJConfig {
+
+    /**
+     * Use this to append additional classpath entries.
+     * This is especially useful if you want your tests to use different
+     * aop.xml files each.
+     */
+    String[] classpathAdditions() default "";
+}
