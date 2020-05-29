3686fec656fc9d96adc030365785a1019ae080a0
bala.narayanasami <bala.narayanasami@flipkart.com>
2020-05-29 13:56:00 +0530

    fix for interception of private methods

--- modules/gojira-core/src/main/java/com/flipkart/gojira/core/aspects/InvocationJoinPointClosure.java
+++ modules/gojira-core/src/main/java/com/flipkart/gojira/core/aspects/InvocationJoinPointClosure.java
@@ -18,42 +18,40 @@
 
 package com.flipkart.gojira.core.aspects;
 
+import java.lang.reflect.AccessibleObject;
 import org.aopalliance.intercept.Invocation;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.reflect.CodeSignature;
 import org.aspectj.lang.reflect.MethodSignature;
 
-import java.lang.reflect.AccessibleObject;
-
 public abstract class InvocationJoinPointClosure extends JoinPointClosure implements Invocation {
 
-    public InvocationJoinPointClosure(JoinPoint jp) {
-        super(jp);
-    }
+  public InvocationJoinPointClosure(JoinPoint jp) {
+    super(jp);
+  }
 
-    /**
-     * @see org.aopalliance.intercept.Joinpoint#getStaticPart()
-     */
-    public AccessibleObject getStaticPart() {
-        CodeSignature cSig = (CodeSignature) jp.getSignature();
-        Class clazz = cSig.getDeclaringType();
-        AccessibleObject ret = null;
-        try {
-            if (cSig instanceof MethodSignature) {
-                ret = clazz.getMethod(cSig.getName(), cSig.getParameterTypes());
-            }
-        } catch (NoSuchMethodException mEx) {
-            throw new UnsupportedOperationException(
-                    "Can't find member " + cSig.toLongString());
-        }
-        return ret;
+  @Override
+  public AccessibleObject getStaticPart() {
+    CodeSignature codeSignature = (CodeSignature) jp.getSignature();
+    Class clazz = codeSignature.getDeclaringType();
+    AccessibleObject ret = null;
+    try {
+      if (codeSignature instanceof MethodSignature) {
+        ret = clazz.getMethod(codeSignature.getName(), codeSignature.getParameterTypes());
+      }
+    } catch (NoSuchMethodException e) {
+      try {
+        ret = clazz.getDeclaredMethod(codeSignature.getName(), codeSignature.getParameterTypes());
+      } catch (NoSuchMethodException e1) {
+        throw new UnsupportedOperationException(
+            "Can't find member " + codeSignature.toLongString());
+      }
     }
+    return ret;
+  }
 
-    /**
-     * @see org.aopalliance.intercept.Invocation#getArguments()
-     */
-    public Object[] getArguments() {
-        return jp.getArgs();
-    }
-
+  @Override
+  public Object[] getArguments() {
+    return jp.getArgs();
+  }
 }
